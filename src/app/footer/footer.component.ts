3260bbdd1d736d28d1fed0a83e3ea6ff92cf5adb
Matthias Berthold <matthiasberthold33@gmail.com>
2023-12-14 07:50:38 +0100

    Contact Form in progress

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,8 +3,54 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  clickedInputs: boolean[] = [false, false, false];
+  showCheckImage: boolean[] = [false, false, false];
+  showMissingImage: boolean[] = [true, true, true];
+  btnEnabled: boolean = true; 
 
+
+  onInputClick(index: number): void {
+    this.clickedInputs.fill(false);
+    this.clickedInputs[index] = true;
+  }
+
+  onInputBlur(index: number): void {  
+    this.clickedInputs.fill(false);
+  }
+
+  onInputChange(event: Event, index: number): void  {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+    if (index === 1) { 
+      this.showCheckImage[index] = value.includes('@');
+      this.showMissingImage[index] = !this.showCheckImage[index];
+    } else {
+      this.showCheckImage[index] = value.trim() !== '';
+      this.showMissingImage[index] = value.trim() === '';
+    }
+  }
+
+  onInputFocus(index: number): void {
+    this.clickedInputs.fill(false);
+    this.clickedInputs[index] = true;
+  }
+
+  checkImageSource(index: number): string {
+    return this.showMissingImage[index] ? 'assets/img/missing.png' : 'assets/img/check.png';
+  }
+
+  enableBtn(): void {
+    this.btnEnabled = true;
+  }
+
+  disableBtn(): void {
+    this.btnEnabled = false;
+  }
+
+  sendMessage() {
+    
+  }
 }
