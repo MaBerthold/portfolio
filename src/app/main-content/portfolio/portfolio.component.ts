411ddaeb8ac4d2cb35f5617aed1e49868b5356d9
Matthias Berthold <matthiasberthold33@gmail.com>
2024-01-04 15:35:10 +0100

    about me responsive

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
@@ -8,4 +8,24 @@ import { Component } from '@angular/core';
 export class PortfolioComponent {
   showDescriptionLeft: boolean = false;
   showDescriptionRight: boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    this.checkVisibility();
+  }
+
+  checkVisibility() {
+    this.checkVisibilityForProject('.project-div.left-visible', 'showDescriptionLeft');
+    this.checkVisibilityForProject('.project-div.right-visible', 'showDescriptionRight');
+  }
+
+  checkVisibilityForProject(selector: string, propertyName: string) {
+    const element = document.querySelector(selector);
+    if (element) {
+      const boundingClientRect = element.getBoundingClientRect();
+      const isVisible = boundingClientRect.top >= 0 && boundingClientRect.bottom <= window.innerHeight * 0.8;
+      if (isVisible) {
+        this[propertyName] = true;
+      }    }
+  }
 }
